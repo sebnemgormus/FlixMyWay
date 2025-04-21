9f69da0d78d265e54720bccce2be158943786295
sebnem <sebnemgormus@gmail.com>
2025-04-21 13:07:55 +0300

    Add social media links to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,42 @@
-import React from 'react'
-import './Footer.css'
-import youtube_icon from '../../assets/youtube_icon.png'
-import twitter_icon from '../../assets/twitter_icon.png'
-import instagram_icon from '../../assets/instagram_icon.png'
-import facebook_icon from '../../assets/facebook_icon.png'
+import React from "react";
+import "./Footer.css";
+import youtube_icon from "../../assets/youtube_icon.png";
+import twitter_icon from "../../assets/twitter_icon.png";
+import instagram_icon from "../../assets/instagram_icon.png";
+import facebook_icon from "../../assets/facebook_icon.png";
 
 const Footer = () => {
   return (
-    <div className='footer'>
+    <div className="footer">
       <div className="footer-icons">
-        <img src={facebook_icon} alt="facebook icon" />
-        <img src={youtube_icon} alt="youtube icon" />
-        <img src={instagram_icon} alt="instagram icon" />
-        <img src={twitter_icon} alt="twitter icon" />
+        <a
+          href="https://www.facebook.com/netflixus"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={facebook_icon} alt="facebook icon" />
+        </a>
+        <a
+          href="https://www.youtube.com/user/NewOnNetflix"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={youtube_icon} alt="youtube icon" />
+        </a>
+        <a
+          href="https://www.instagram.com/netflix/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={instagram_icon} alt="Instagram icon" />
+        </a>
+        <a
+          href="https://twitter.com/netflix"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={twitter_icon} alt="twitter icon" />
+        </a>
       </div>
       <ul>
         <li>Audio Description</li>
@@ -28,9 +52,9 @@ const Footer = () => {
         <li>Corporate Information</li>
         <li>Contact Us</li>
       </ul>
-      <p className='copyright-text'>© 1997-2025 Netflix, Inc.</p>
+      <p className="copyright-text">© 1997-2025 Netflix, Inc.</p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
